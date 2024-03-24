d770857e7d8f680469d5a469e8e98b76b7bb7d3d
“jeffe419” <jeffersonatigah2000@gmail.com>
2024-03-24 18:11:33 +0000

    MobileViewFix

--- atigahweb/src/components/Resume.js
+++ atigahweb/src/components/Resume.js
@@ -1,27 +1,40 @@
-import React from "react";
-import { Button, Container, Row, Col } from "react-bootstrap";
-import ProfilePicture from "../images/ProfilePicture.jpg";
-import "../styles/AboutSection.css";
-
+import React, { useState, useEffect } from "react";
+import { Button, Container } from "react-bootstrap";
+import "../styles/AboutSection.css"; // Assuming min-full-height is added here
+import { useNavigate } from "react-router-dom";
 
 const Resume = () => {
- 
-  const resumePdfUrl = '/jeffresume.pdf'; 
-  
+  const navigate = useNavigate();
+  const resumePdfUrl = '/jeffresume.pdf';
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768); // Or your preferred mobile breakpoint
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
-    <Container className="my-5">
-     
-      <div className="d-flex justify-content-center">
-        <iframe 
-          src={resumePdfUrl}
-          
-          width="100%" 
-          height="700px" 
-          title="Resume" 
-          className="resume-iframe" 
-        >
-        </iframe>
-      </div>
+    <Container className="my-5 min-full-height">
+      {isMobile ? (
+        <div className="text-center">
+          <Button variant="primary" href={resumePdfUrl} download="JeffResume.pdf">Download Resume</Button>
+        </div>
+      ) : (
+        <div className="d-flex justify-content-center">
+          <iframe
+            src={resumePdfUrl}
+            width="100%"
+            height="700px"
+            title="Resume"
+            className="resume-iframe"
+          ></iframe>
+        </div>
+      )}
     </Container>
   );
 };
